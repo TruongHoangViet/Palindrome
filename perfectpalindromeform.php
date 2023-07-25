58d3f76e168ae5248d96c26b47054e2a04047c92
TruongHoangViet <89001506+TruongHoangViet@users.noreply.github.com>
2023-07-25 10:58:47 +0700

    Update perfectpalindromeform.php

--- perfectpalindromeform.php
+++ perfectpalindromeform.php
@@ -8,7 +8,7 @@
   <title>TITLE</title> 
 </head> 
 <body> 
-<h1>Web Programming Form Hoang Viet Truong</h1>
+<h1>Web Programming Form team 3</h1>
 <form action = "perfectpalindrome.php" method = "post" > 
   Enter word: <input type="string" name ="stringtest">
 <input type="submit">
